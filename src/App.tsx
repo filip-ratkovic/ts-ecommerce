4c2dd88f0f96a27708fc42dcc108cb9e5b92d944
Filip Ratkovic <filip28r@gmail.com>
2023-08-30 13:42:53 +0200

    Nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import "./app.css"
+import { Container } from 'react-bootstrap'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Products from './pages/Products/Products'
+import SingleProduct from './pages/SingleProduct/SingleProduct'
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Nav/>
+    <Container className='mb-4'>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/products' element={<Products/>}/>
+      <Route path='/products/:id' element={<SingleProduct/>}/>
+      </Routes>
+    </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
